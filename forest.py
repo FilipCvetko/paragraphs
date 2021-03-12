61181d6c0cf7f7c6342a51329f0c4939a6e79c17
Filip Cvetko <filipcvetk123@gmail.com>
2021-03-12 19:19:28 +0100

    Forest and TF-IFD added in forest.py
    Currently lacks functionality. The output prediction appears pretty much random.

--- forest.py
+++ forest.py
@@ -1,64 +1,101 @@
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from medwise_utils import *
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import nltk
 from tqdm import tqdm
 import time
+import random
+from config import *
 
-# Load dataset
-data = pd.read_csv("./data/data.csv")
-data_list = data["Text"].tolist()
+class ForestClassifier():
 
-test = pd.read_csv("./data/test.csv")
+    def __init__(self, data_path, test_path):
+        self.data = pd.read_csv(data_path)
+        self.headings = self.data["Heading"].tolist()
+        self.data = self.data["Text"].tolist()
+        self.test = pd.read_csv(test_path).dropna(how="all")
+        self.test = self.test.sample(frac=1)
+        self.sleep_time = 3
+        
 
-headings_list = data["Heading"].tolist()
-Y = []
+    def preprocess_headings(self):
 
-for item in headings_list:
-    headings = get_headings(item)
-    temp = 0
-    if "Overview" in headings:
-        temp = 1
-    if "Presentation" in headings:
-        temp = 2
-    if "Diagnosis" in headings:
-        temp = 3
-    if "Management" in headings:
-        temp = 4
-    if "Follow up" in headings:
-        temp = 5
-    if "Others" in headings:
-        temp = 6
+        new_list = []
+        for item in self.headings:
+            headings = get_headings(item)
+            temp = 0
+            if "Overview" in headings:
+                temp = 1
+            if "Presentation" in headings:
+                temp = 2
+            if "Diagnosis" in headings:
+                temp = 3
+            if "Management" in headings:
+                temp = 4
+            if "Follow up" in headings:
+                temp = 5
+            if "Others" in headings:
+                temp = 6
 
-    Y.append(temp)
+            new_list.append(temp)
 
-ps = PorterStemmer()
+        self.headings = new_list
+        return new_list
 
-# for ind, val in tqdm(enumerate(data_list)):
-#     tokens = nltk.word_tokenize(val)
-#     new_tokens = []
-    
-#     for token in tokens:
-#         if token not in set(stopwords.words("english")) and token.isalnum():
-#             token = ps.stem(token)
-#             new_tokens.append(token)
+    def preprocess_text(self):
 
-#     data_list[ind] = new_tokens
+        stemmer = WordNetLemmatizer()
 
-vectorizer = CountVectorizer(min_df=1)
-X = vectorizer.fit_transform(data_list).toarray()
+        for ind, val in tqdm(enumerate(self.data)):
+            tokens = nltk.word_tokenize(val)
+            new_tokens = []
+            
+            for token in tokens:
+                if token not in set(stopwords.words("english")) and token.isalnum():
+                    token = stemmer.lemmatize(token)
+                    new_tokens.append(token)
 
-clf = RandomForestClassifier(max_depth=20)
-clf.fit(X,Y)
+            self.data[ind] = " ".join(new_tokens)
+        
+        return self.data
 
-for i, item in enumerate(test["Text"]):
-    print(item)
-    print("--------------------------")
-    print("Predicted: ", clf.predict(vectorizer.transform([item]).toarray()))
-    print("Actual: ", test.iloc[i]["Heading"])
-    print("--------------------------")
-    time.sleep(2)
+    def forest_classify(self):
 
+        vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+        X = vectorizer.fit_transform(self.data).toarray()
+
+        clf = RandomForestClassifier(max_depth=100)
+        clf.fit(X,self.headings)
+
+        for i, item in enumerate(self.test["Text"]):
+            print(item)
+            print("--------------------------")
+            print("Predicted: ", clf.predict(vectorizer.transform([item]).toarray()))
+            print("Actual: ", self.test.iloc[i]["Heading"])
+            print("--------------------------")
+            time.sleep(self.sleep_time)
+
+    def tfid_classify(self):
+
+        tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+        X = tfidfconverter.fit_transform(self.data).toarray()
+
+        clf = RandomForestClassifier(max_depth=100)
+        clf.fit(X,self.headings)
+
+        for i, item in enumerate(self.test["Text"]):
+            print(item)
+            print("--------------------------")
+            print("Predicted: ", clf.predict(tfidfconverter.transform([item]).toarray()))
+            print("Actual: ", self.test.iloc[i]["Heading"])
+            print("--------------------------")
+            time.sleep(self.sleep_time)
+
+forest = ForestClassifier(data_dir, test_dir)
+forest.preprocess_headings()
+forest.preprocess_text()
+forest.forest_classify()
