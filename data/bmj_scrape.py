8673042915b13a56bac39a61b93d255215be3b2b
Filip Cvetko <filipcvetk123@gmail.com>
2021-03-21 22:04:42 +0100

    Improved content scraping functionality.
    chunkText function has also been added to restrict content to a
    max_tokens number of tokens - default 512 for BERT.

--- data/bmj_scrape.py
+++ data/bmj_scrape.py
@@ -8,7 +8,8 @@ ROOT_URL = "https://bestpractice.bmj.com"
 SPECIALTY_URL ="https://bestpractice.bmj.com/specialties/1/Allergy-and-immunology"
 DISEASE_URL = "https://bestpractice.bmj.com/topics/en-gb/596"
 MENU_URL = "https://bestpractice.bmj.com/topics/en-gb/3000117/treatment-algorithm"
-CONTENT_THRESHOLD = 3
+MENU_URLL = "https://bestpractice.bmj.com/topics/en-gb/3000117/differentials"
+CONTENT_THRESHOLD = 1
 
 def specialties(url):
     """
@@ -65,26 +66,42 @@ def find_content(menu_url):
     soup = BeautifulSoup(source, "html.parser")
 
     content = []
+    chunked_content = []
 
     # Remove all references
     for span in soup.find_all("span", {"class" : "reference"}):
         span.decompose()
 
     # Find "poce contents in class"
+    soup = soup.find("div", attrs={"class" : re.compile("poce_contents")})
+    print(soup["class"])
 
-    divs = soup.find_all("div")
-    for div in divs:
-        num_of_content = 0
-        for child in div.children:
-            if child.name == 0:
-                break
-            if child.name == "p" or child.name == "ul":
-                num_of_content += 1
-
-        if num_of_content > CONTENT_THRESHOLD:
-            corrected_text = preprocess_text(str(div.text))
-            content.append(corrected_text)
+    paragraphs = soup.find_all("p")
+    for p in paragraphs:
+        content.append(p.text)
 
     return content
 
-print(find_content(MENU_URL)[0])
+def chunkText(text, max_tokens=512):
+    """
+        Content comes in list form. A list of all paragraph texts.
+    """
+    chunked_content = []
+    num_tokens = 0
+    new_content = ""
+
+    for i, c in enumerate(text):
+        new_tokens = len(c.split())
+        num_tokens += new_tokens
+        if num_tokens < max_tokens and i == len(text) - 1:
+            chunked_content.append(new_content)
+        elif num_tokens < max_tokens:
+            new_content += (c + " ")
+        else:
+            chunked_content.append(new_content)
+            num_tokens = 0
+            new_content = ""
+
+    return chunked_content
+
+print(find_content(MENU_URL))
