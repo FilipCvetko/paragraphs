fed874ea32b0569b45371cd90e57d2869f5ee6e5
Filip Cvetko <filipcvetk123@gmail.com>
2021-03-28 10:41:06 +0200

    Miscellaneous.

--- config.py
+++
@@ -1,7 +0,0 @@
-unique_master_headings = ["Overview", "Presentation", "Diagnosis", "Management", "Follow up", "Others"]
-
-# Threshold applies to verbs. They should be more important.
-threshold = 5
-
-data_dir = "./data/data.csv"
-test_dir = "./data/test.csv"
